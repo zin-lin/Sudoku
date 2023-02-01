cf845bfcebcf7de24ee96bbcf96a323af8401c83
zin-htun <zinlinhtun34@gmail.com>
2023-02-01 02:50:10 +0000

    New Commits

--- src/main/java/com/napier/sudoku/SudokuEngine.java
+++ src/main/java/com/napier/sudoku/SudokuEngine.java
@@ -26,23 +26,25 @@ public class SudokuEngine {
         String upperBound = "╔═══╗";
         String midBound   = "║   ║";
         String lowerBound = "╚═══╝";
+        String blue = "\u001B[34m";
 
         this.xAxis = xAxis; this.yAxis = yAxis; // set things
         for (int i = 0; i < xAxis; i++)
         {
             for (int i1 = 0; i1 < yAxis; i1++)
             {
-                System.out.print("╔═══╗");
+
+                System.out.print("╔═══╗" + blue);
             }
             System.out.print("\n");
             for (int i1 = 0; i1 < xAxis; i1++)
             {
-                System.out.print("║   ║");
+                System.out.print("║   ║" + blue);
             }
             System.out.print("\n");
             for (int i1 = 0; i1 < yAxis; i1++)
             {
-                System.out.print("╚═══╝");
+                System.out.print("╚═══╝" + blue);
             }
             System.out.print("\n");
 
