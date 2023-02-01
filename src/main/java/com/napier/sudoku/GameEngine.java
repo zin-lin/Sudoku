8a5dfcfed9492ecb6e51834f91034829df3125bc
zin-htun <zinlinhtun34@gmail.com>
2023-02-01 04:35:24 +0000

    randomizer added

--- src/main/java/com/napier/sudoku/GameEngine.java
+++ src/main/java/com/napier/sudoku/GameEngine.java
@@ -4,6 +4,7 @@ public class GameEngine {
 
     public static void main (String [] args){
         SudokuEngine sudokuEngine = new SudokuEngine(9,9); // Standard Sudoku
+
     }
 
 }
--- src/main/java/com/napier/sudoku/SudokuEngine.java
+++ src/main/java/com/napier/sudoku/SudokuEngine.java
@@ -1,22 +1,31 @@
 package com.napier.sudoku;
 
+// Class SudokuEngine
+// Author : Zin Lin Htun
+// @matric : 40542237@live.napier.ac.uk
 public class SudokuEngine {
 
+    //private materials
     private int xAxis;
     private int yAxis;
 
+    // getters and setters
+    // setXAxis to set xAxis
     public void setXAxis(int xAxis) {
         this.xAxis = xAxis;
     }
 
+    // getXAxis to get xAxis
     public int getXAxis() {
         return xAxis;
     }
 
+    // setYAxis to set yAxis
     public void setYAxis(int yAxis) {
         this.yAxis = yAxis;
     }
 
+    // getYAxis to set yAxis
     public int getYAxis() {
         return yAxis;
     }
@@ -24,26 +33,30 @@ public class SudokuEngine {
     // Public Constructor
     public SudokuEngine(int xAxis, int yAxis){
         String upperBound = "╔═════╗ ";
-        String midBound   = "║     ║ ";
+        String leftBound  = "║  ";
+        String rightBound = "  ║ ";
         String lowerBound = "╚═════╝ ";
         String blue = "";
 
         this.xAxis = xAxis; this.yAxis = yAxis; // set things
-        for (int i = 0; i < xAxis; i++)
+        // write rows
+        for (int i = 0; i < yAxis; i++)
         {
-            for (int i1 = 0; i1 < yAxis; i1++)
+            // each line is written
+            for (int i1 = 0; i1 < xAxis; i1++)
             {
                 String ending = (i1+1) %3 == 0? "  ":"";
                 System.out.print(blue+ upperBound + ending);
             }
             System.out.print("\n");
-            for (int i1 = 0; i1 < xAxis; i1++)
+            for (int i1 = 0; i1 < yAxis; i1++)
             {
+                String randomizer = "_";
                 String ending = (i1+1) %3 == 0? "  ":"";
-                System.out.print(midBound + ending);
+                System.out.print(leftBound + randomizer + rightBound + ending);
             }
             System.out.print("\n");
-            for (int i1 = 0; i1 < yAxis; i1++)
+            for (int i1 = 0; i1 < xAxis; i1++)
             {
                 String ending = (i1+1) %3 == 0? "  ":"";
                 System.out.print(lowerBound + ending);
@@ -53,14 +66,11 @@ public class SudokuEngine {
             if ((i+1) % 3 == 0 ){
                 System.out.print("\n");
             }
-
         }
-
     }
-
     // Default Constructor
     public SudokuEngine(){
-
+        
     }
 
 }
